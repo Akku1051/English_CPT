ebbdd56fcad88303a0e36af7849d2bf64e4a08fa
Akku1051 <110792949+Akku1051@users.noreply.github.com>
2025-01-11 17:11:48 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function intro() {
+    window.location.href = "index.html";
+    console.log("Intro page opened");
+}
+
+function macbeth() {
+    window.location.href = "macbeth.html";
+    console.log("Macbeth page opened");
+}
+
+function indian() {
+    window.location.href = "indian_horse.html";
+    console.log("Indian Horse page opened");
+}
+
+function two() {
+    window.location.href = "two_kinds.html";
+    console.log("Two Kinds page opened");
+}
+
+function connections() {
+    window.location.href = "universal.html";
+    console.log("Universal Connection page opened");
+}
